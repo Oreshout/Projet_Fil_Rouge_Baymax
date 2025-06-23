6ff2a2a5320e0960ecaf7de91c0c79817495d53a
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 12:00:50 +0200

    blink en plus

--- Pole_Info/preparation.c
+++ Pole_Info/preparation.c
@@ -11,7 +11,20 @@
 #include <string.h>
 #include <stdbool.h>
 
+void LED_blink(LED *self, int count, float cycleTime)
+{
+    assert(self && self->m_pi >= 0 && "The LED must be initialized");
 
+    // TODO : initialiser correctement les champs de la structure pour lancer un clignotement
+    self->m_accu = 0;
+    self->m_cycleCount = count*2;
+    self->m_cycleTime = (cycleTime*1000000)/2;
+    self->m_cycleIndex = 0;
+    // TODO : allumer la led (mode PI_HIGH)
+    
+    gpio_write(self -> m_pi, self -> m_gpio, PI_HIGH);
+    self->m_prevUpdateTick = get_current_tick(self->m_pi); 
+}
 
 bool LED_isBlinking(LED *self)  // LED pin : 21
 {
