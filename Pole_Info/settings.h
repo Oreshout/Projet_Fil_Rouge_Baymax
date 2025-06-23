d9e582c7a3b9d0abd83793050fe2501bed39e251
fayroth92 <ludwiczak.joachim@gmail.com>
2025-06-23 13:46:24 +0200

    clear settings.h

--- Pole_Info/settings.h
+++ Pole_Info/settings.h
@@ -40,10 +40,6 @@ typedef struct Button
     int m_prevCbCount;
     int m_currCbCount;
 } Button;
-void Button_init(Button *self, int pi, int gpio);
-void Button_quit(Button *self);
-void Button_update(Button *self);
-bool Button_isPressed(Button *self);
 
 // ===================== LED =====================
 typedef struct LED
@@ -56,11 +52,6 @@ typedef struct LED
     int m_cycleCount;
     int m_cycleIndex;
 } LED;
-void LED_init(LED *self, int pi, int gpio);
-void LED_quit(LED *self);
-void LED_update(LED *self);
-void LED_blink(LED *self, int count, float cycleTime);
-bool LED_isBlinking(LED *self);
 
 // ===================== MotorController =====================
 typedef struct MotorController
@@ -85,24 +76,3 @@ typedef struct MotorController
     int m_errorCount;
     float m_saturationTime;
 } MotorController;
-void MotorController_init(MotorController *self, int pi, int gpioForward, int gpioBackward, int gpioControl);
-void MotorController_quit(MotorController *self);
-void MotorController_update(MotorController *self);
-void MotorController_stop(MotorController *self);
-void MotorController_setTargetSpeed(MotorController *self, float speed);
-void MotorController_setController(MotorController *self, float kp, float ki);
-void MotorController_setStartPower(MotorController *self, int startPower);
-void MotorController_setBackward(MotorController *self, bool goBackward);
-float MotorController_getSpeed(MotorController *self);
-float MotorController_getDistance(MotorController *self);
-void turn(MotorController *left, MotorController *right, int slotCount, float speed);
-void drive(MotorController *left, MotorController *right, int slotCount, float speed);
-
-
-// ===================== Tools =====================
-int Int_clampAB(int value, int a, int b);
-float Float_clamp01(float value);
-float Float_clampAB(float value, float a, float b);
-float Float_lerp(float value0, float value1, float t);
-float Float_invLerp(float value0, float value1, float value);
-float Float_remap(float src0, float src1, float dst0, float dst1, float value);
