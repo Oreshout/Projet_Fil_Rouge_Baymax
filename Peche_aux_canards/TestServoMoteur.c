54183da67a88c151b99cb4aacf58d6cdc71c4330
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 22:58:38 +0200

    Reglage error pinL & pinR

--- Peche_aux_canards/TestServoMoteur.c
+++ Peche_aux_canards/TestServoMoteur.c
@@ -4,7 +4,7 @@ int pi;
 
 void testR(int pi)
 {
-    set_mode(pi, servo_pin, PI_OUTPUT);
+    set_mode(pi, servo_pinR, PI_OUTPUT);
     set_servo_pulsewidth(pi, servo_pinR, 1000); // Position initiale
     sleep(5);
     set_servo_pulsewidth(pi, servo_pinR, 500); // Position finale
@@ -12,7 +12,7 @@ void testR(int pi)
 
 void testL(int pi)
 {
-    set_mode(pi, servo_pin, PI_OUTPUT);
+    set_mode(pi, servo_pinL, PI_OUTPUT);
     set_servo_pulsewidth(pi, servo_pinL, 1000); // Position initiale
     sleep(5);
     set_servo_pulsewidth(pi, servo_pinL, 500); // Position finale
@@ -21,7 +21,8 @@ void testL(int pi)
 int main()
 {
     pi = pigpio_start(NULL, NULL); // Démarre la bibliothèque pigpio pour le contrôle des GPIO
-    testR(pi); // Appelle la fonction de test
+    testR(pi); // Appelle la fonction de test Right
+    testL(pi); // Appelle la fonction de test Left
     
     return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
