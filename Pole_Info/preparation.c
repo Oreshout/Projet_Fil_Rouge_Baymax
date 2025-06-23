9e9d1a0f7f8764b45ec1fbf348956e25c3ca68f4
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 11:58:50 +0200

    Ajout LED_isblinking !

--- Pole_Info/preparation.c
+++ Pole_Info/preparation.c
@@ -10,3 +10,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+
+
+bool LED_isBlinking(LED *self)  // LED pin : 21
+{
+    assert(self && self->m_pi >= 0 && "The LED must be initialized");
+
+    // TODO : vérifier si les cycles sont terminés
+    if(self ->  m_cycleIndex < self ->  m_cycleCount)
+    {
+        return true;
+    
+    }
+    return false;
+}
