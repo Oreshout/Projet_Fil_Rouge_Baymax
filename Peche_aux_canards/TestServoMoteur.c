005c269052f5fad43b124d9f9bc2913e9ce5427b
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 23:17:26 +0200

    ServoMoteur Miroir

--- Peche_aux_canards/TestServoMoteur.c
+++ Peche_aux_canards/TestServoMoteur.c
@@ -2,27 +2,24 @@
 
 int pi;
 
-void testR(int pi)
+void test(int pi)
 {
+    int val = 1000;
+    int val_miroir = 2000 - (val - 1000); 
     set_mode(pi, servo_pinR, PI_OUTPUT);
-    set_servo_pulsewidth(pi, servo_pinR, 1000); // Position initiale
-    sleep(5);
-    set_servo_pulsewidth(pi, servo_pinR, 500); // Position finale
-}
-
-void testL(int pi)
-{
     set_mode(pi, servo_pinL, PI_OUTPUT);
-    set_servo_pulsewidth(pi, servo_pinL, 1000); // Position initiale
+    set_servo_pulsewidth(pi, servo_pinR, 1000); // Position initiale // ! RIGHT
+    set_servo_pulsewidth(pi, servo_pinL, val_miroir); // Position initiale // ! LEFT
     sleep(5);
-    set_servo_pulsewidth(pi, servo_pinL, 500); // Position finale
+    set_servo_pulsewidth(pi, servo_pinR, 500); // Position finale // ! RIGHT
+    set_servo_pulsewidth(pi, servo_pinL, 500); // Position finale // ! LEFT
+
 }
 
 int main()
 {
     pi = pigpio_start(NULL, NULL); // Démarre la bibliothèque pigpio pour le contrôle des GPIO
-    testR(pi); // Appelle la fonction de test Right
-    testL(pi); // Appelle la fonction de test Left
-    
+    test(pi); // Appelle la fonction de test Right
+  
     return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
