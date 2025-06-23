6c9fe16cdcf38660a9abf0881f983a70718f4bf3
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 22:34:09 +0200

    Modification TestServoMoteur
    IL MARCHE a voir avec les bon angles

--- Peche_aux_canards/TestServoMoteur.c
+++ Peche_aux_canards/TestServoMoteur.c
@@ -1,16 +1,18 @@
 #include "settings.h"
 
-int pi = pigpio_start(NULL, NULL); // Démarre la bibliothèque pigpio
+int pi;
 
 void test(pi)
 {
     set_mode(pi, servo_pin, PI_OUTPUT);
     set_servo_pulsewidth(pi, servo_pin, 1000); // Position initiale
     sleep(5);
+    set_servo_pulsewidth(pi, servo_pin, 500); // Position finale
 }
 
-int main(pi)
+int main()
 {
+    pi = pigpio_start(NULL, NULL); // Démarre la bibliothèque pigpio pour le contrôle des GPIO
     test(); // Appelle la fonction de test
     
     return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
