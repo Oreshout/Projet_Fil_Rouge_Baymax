f3dad8db7ce02300a26ea35269ef4044c95f58af
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-23 12:10:04 +0200

    Changement FULL
    supp blink and isblinking ajout de update led et bouton et setup des pin

--- Pole_Info/preparation.c
+++ Pole_Info/preparation.c
@@ -3,7 +3,8 @@
 *
 *      PREPARATION A FAIRE JOUR 1 
 *
-*
+*       LED pin : 21
+*       Bouton pin : 20
 *
 */
 #include <stdio.h>
@@ -11,30 +12,37 @@
 #include <string.h>
 #include <stdbool.h>
 
-void LED_blink(LED *self, int count, float cycleTime)
+void LED_update()
 {
-    assert(self && self->m_pi >= 0 && "The LED must be initialized");
+    int boucle = 0;
+    set_mode(pi, 21, PI_OUTPUT); // Met le GPIO 21 en mode sortie pour la LED
 
-    // TODO : initialiser correctement les champs de la structure pour lancer un clignotement
-    self->m_accu = 0;
-    self->m_cycleCount = count*2;
-    self->m_cycleTime = (cycleTime*1000000)/2;
-    self->m_cycleIndex = 0;
-    // TODO : allumer la led (mode PI_HIGH)
-    
-    gpio_write(self -> m_pi, self -> m_gpio, PI_HIGH);
-    self->m_prevUpdateTick = get_current_tick(self->m_pi); 
-}
-
-bool LED_isBlinking(LED *self)  // LED pin : 21
-{
-    assert(self && self->m_pi >= 0 && "The LED must be initialized");
-
-    // TODO : vérifier si les cycles sont terminés
-    if(self ->  m_cycleIndex < self ->  m_cycleCount)
+    while (boucle < 5)
     {
-        return true;
-    
+        gpio_write(pi, 21, PI_HIGH); // Envoi un signal "HAUT" sur le GPIO 16 pour l'allumer
+        sleep(1);
+        gpio_write(pi, 21, PI_LOW); // Envoi un signal "BAS" sur le GPIO16 pour l'éteindre
+        sleep(1);
+        boucle++;
     }
-    return false;
 }
+
+void Bouton_update()
+{
+    set_mode(pi, 20, PI_INPUT); // Met le GPIO 20 en mode entrée
+    set_pull_up_down(pi, 20, PI_PUD_DOWN); // Active la résistance de rappel sur le GPIO 20
+
+    while (1) 
+    {
+        int state = gpio_read(pi, 20); // Lecture de l'état du bouton poussoir
+        if (state == PI_HIGH) { // Si le GPIO a reçu un "HAUT"
+            printf("Le bouton poussoir sur le GPIO 20 est enclenché\n");
+        } else if (state == PI_LOW) { // Si le GPIO a reçu un "BAS"
+            printf("Le bouton poussoir sur le GPIO 20 est relaché\n");
+        } else { // Ne devrait pas exister
+            printf("Le GPIO 16 est dans un état inconnu\n");
+        }
+        sleep(1);
+    }
+}
+
