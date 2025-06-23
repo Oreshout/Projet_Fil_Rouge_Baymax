e8f7f18771ff444aa93d64104cf34b8a2f967801
fayroth92 <ludwiczak.joachim@gmail.com>
2025-06-23 16:37:30 +0200

    update setings

--- Pole_Info/settings.h
+++ Pole_Info/settings.h
@@ -12,6 +12,7 @@
 #include <pigpiod_if2.h>
 #include <fcntl.h>
 #include "esieabot-ai.h"
+#include <esieabot.h>
 
 //------------------------------------------------------------------------------
 // GPIO definitions
@@ -32,53 +33,6 @@
 #define Bouton_pin 21
 
 #define servo_pin 13
-
-// ===================== Button =====================
-typedef struct Button
-{
-    int m_pi;
-    int m_gpio;
-    int m_callbackID;
-    bool m_isPressed;
-    int m_prevCbCount;
-    int m_currCbCount;
-} Button;
-
-// ===================== LED =====================
-typedef struct LED
-{
-    int m_pi;
-    int m_gpio;
-    uint32_t m_accu;
-    uint32_t m_prevUpdateTick;
-    uint32_t m_cycleTime;
-    int m_cycleCount;
-    int m_cycleIndex;
-} LED;
-
-// ===================== MotorController =====================
-typedef struct MotorController
-{
-    int m_pi;
-    int m_gpioControl;
-    int m_gpioForward;
-    int m_gpioBackward;
-    int m_gpioMotor;
-    int m_callbackID;
-    int m_cbCount;
-    int m_controllerCbCount;
-    float m_kp;
-    float m_ki;
-    float m_integral;
-    float m_targetSpeed;
-    float m_speed;
-    int m_power;
-    int m_startPower;
-    uint32_t m_prevCbTick;
-    uint32_t m_prevUpdateTick;
-    int m_errorCount;
-    float m_saturationTime;
-} MotorController;
 
 // ===================== Marker =====================
 
