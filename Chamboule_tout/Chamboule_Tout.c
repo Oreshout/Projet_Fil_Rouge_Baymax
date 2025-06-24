ae5ebbd89c78b4ec4ca4e4016a976ab152289ef1
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-24 09:19:50 +0200

    call

--- Chamboule_tout/Chamboule_Tout.c
+++ Chamboule_tout/Chamboule_Tout.c
@@ -2,5 +2,12 @@
 *
 *      CODE : Chamboule TOUT
 *
+*      ! Detecter les différents marqueurs et viser les goblets les plus bas pour en faire tomber le plus
+*      ! possible 
+*
+*
+*
+*
+*
 *
 */
--- Peche_aux_canards/Peche_aux_canards.c
+++ Peche_aux_canards/Peche_aux_canards.c
@@ -20,9 +20,22 @@ int DetectionMarker()
     return distance;
 }
 
+bool DetectionMarkerExist()
+{
+    struct marker *DetectionMarker = getmarkers(30);
+    
+    if (DetectionMarker->id != -1) // Si un marqueur est détecté
+    {
+        printf("Marqueur trouvé: numéro %d à %dcm de distance. x=%d y=%d\n", DetectionMarker->id, DetectionMarker->z, DetectionMarker->x, DetectionMarker->y);
+        return true;
+    }
+    
+    return false;
+}
+
 void GestionBras()
 {
-    
+
 }
 
 void GestionServoMoteurHaut()
@@ -82,8 +95,33 @@ void GestionAttrape()
     }
 }
 
+void GestionMouvementRobot(MotorController motorL, MotorController motorR)
+{
+  
+}
+
 int main()
 {
     pi = pigpio_start(NULL, NULL);
+
+    float kp = 4.0f; // Valeurs conseillées pour le contrôleur PI
+    float ki = 2.0f;
+
+    float speed = 50.f;
+
+    MotorController motorL; // Création de l'objet contrôleur moteur
+    MotorController_init(&motorL, pi, GPIO_FORWARD_L, GPIO_BACKWARD_L, GPIO_MOTOR_CONTROL_L); // Initialisation du contrôleur moteur
+
+    MotorController motorR; // Création de l'objet contrôleur moteur
+    MotorController_init(&motorR, pi, GPIO_FORWARD_R, GPIO_BACKWARD_R, GPIO_MOTOR_CONTROL_R); // Initialisation du contrôleur moteur
+
+    MotorController_setStartPower(&motorL, 110);
+    MotorController_setController(&motorL, kp, ki);
+
+    MotorController_setStartPower(&motorR, 110);
+    MotorController_setController(&motorR, kp, ki);
+
+    MotorController_stop(&motorL);
+    MotorController_stop(&motorR);
 
 }
