8d557755d66f640eaf04c73b394845874c5431ab
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-24 11:42:56 +0200

    Bon bidule a la fin

--- Chamboule_tout/Test_Lancer.c
+++ Chamboule_tout/Test_Lancer.c
@@ -5,15 +5,13 @@ int pi;
 void ActivationServoMoteurLancer()
 {
     set_mode(pi, servo_pin_Lancer, PI_OUTPUT); 
-    set_servo_pulsewidth(pi, servo_pin_Lancer, 1000); // Position initiale
+    set_servo_pulsewidth(pi, servo_pin_Recharge, 1000); // Position initiale
     sleep(5); // Attendre que le servo se positionne
-}
-
-void ActivationServoMoteurrecharge()
-{
     set_mode(pi, servo_pin_Recharge, PI_OUTPUT); 
-    set_servo_pulsewidth(pi, servo_pin_Recharge, 1000); // Position finale
+    set_servo_pulsewidth(pi, servo_pin_Lancer, 1000); // Position finale
     sleep(10); // Attendre que le servo se positionne
+    set_servo_pulsewidth(pi, servo_pin_Lancer, 500); // Position finale
+    set_servo_pulsewidth(pi, servo_pin_Recharge, 500);
 }
 
 
@@ -23,12 +21,8 @@ int main()
 
     scanf("Démarrage du test de lancement et de recharge du servo moteur...\n");
 
-    ActivationServoMoteurrecharge(); // Active le servo moteur pour recharger
+    ActivationServoMoteurLancer(); // Active le servo moteur pour recharger
     // Attendre que le servo se positionne
-
-    ActivationServoMoteurLancer(); // Active le servo moteur pour lancer
-    // Attendre que le servo se positionne
- 
 
     pigpio_stop(pi); // Arrête la bibliothèque pigpio
     return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
