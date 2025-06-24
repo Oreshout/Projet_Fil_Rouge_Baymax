ec2d945d7437d395ab2a6ff90b62fba31f29b426
Matt <92225151+Oreshout@users.noreply.github.com>
2025-06-24 09:57:58 +0200

    start CT V.2

--- Chamboule_tout/Chamboule_Tout.c
+++ Chamboule_tout/Chamboule_Tout.c
@@ -14,3 +14,32 @@
 *
 */
 
+int pi;
+
+bool DetectionMarkerExist()
+{
+    struct marker *markers = get_markers(30); // Récupération des marqueurs de 51mm de côté (à modifier en fonction de vos marqueurs !)
+    
+    if (markers->id != -1) // Si un marqueur est détecté
+    {
+        printf("Marqueur trouvé: numéro %d à %dcm de distance. x=%d y=%d\n", markers->id, markers->z, markers->x, markers->y);
+        return true;
+    }
+    
+    return false;
+}
+
+int DetectionMarker()
+{
+    int distance = 0;
+    struct marker *markers = get_markers(30); // Récupération des marqueurs de 51mm de côté (à modifier en fonction de vos marqueurs !)
+    
+    if (markers->id != -1) // Si un marqueur est détecté
+    {
+        distance = markers->z;
+        printf("Marqué à %d cm\n", distance);
+    }
+    
+    return distance;
+}
+
